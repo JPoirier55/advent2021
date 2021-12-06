ebc3367b5e363609861ae98f70addb370ef5a661
jpoirier55 <jake.poirier@stash.com>
2021-12-05 21:53:45 -0500

    Day3 part1 done

--- day3/day3.go
+++ day3/day3.go
@@ -2,34 +2,73 @@ package day3
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 )
 
+func decimalFromBinary(binaryStr string) int64 {
+	if i, err := strconv.ParseInt(binaryStr, 2, 64); err != nil {
+		fmt.Println(err)
+		return 0
+	} else {
+		return i
+	}
+}
+
 func RunPart1(inputLines []string) {
 	totalLines := 0
-	m := make(map[int]int)
+	binMap := make(map[int]int)
 	for _, val := range inputLines {
 		totalLines++
 		indVals := strings.Split(val, "")
 
 		for i, indVal := range indVals {
 			intVal, _ := strconv.Atoi(indVal)
-			m[i] += intVal
+			binMap[i] += intVal
 		}
-		fmt.Println(m)
+		fmt.Println(binMap)
 	}
+	keys := make([]int, 0, len(binMap))
 
-	for i, val := range m {
-		if val > totalLines/2 {
-			fmt.Printf("%d : 1", i)
+	for k, _ := range binMap {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	gamma := ""
+	epsilon := ""
+	for _, k := range keys {
+		if binMap[k] > totalLines/2 {
+			fmt.Printf("%d : 1", k)
+			gamma += "1"
+			epsilon += "0"
 		}else {
-			fmt.Printf("%d : 0", i)
+			fmt.Printf("%d : 0", k)
+			gamma += "0"
+			epsilon += "1"
 		}
 		fmt.Println()
 	}
+	fmt.Println(gamma)
+	fmt.Println(epsilon)
+	gammaDec := decimalFromBinary(gamma)
+	epsilonDec := decimalFromBinary(epsilon)
+	fmt.Println(gammaDec)
+	fmt.Println(epsilonDec)
+	fmt.Println(gammaDec*epsilonDec)
 }
 
 func RunPart2(inputLines []string) {
+	totalLines := 0
+	zeros := make([]string, 1)
+	ones := make([]string, 1)
+	for i, _ := range inputLines[0] {
+		fmt.Println(string(inputLines[0][i]))
+		for _, val := range inputLines {
+			fmt.Println(val)
+		}
+	}
+
+
 
 }
