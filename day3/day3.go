ca2088ce6605fdae804a867a23c272ecf8f5efe2
jpoirier55 <jake.poirier@stash.com>
2021-12-08 21:29:14 -0500

    Pile of garbage I give up

--- day3/day3.go
+++ day3/day3.go
@@ -51,7 +51,9 @@ func RunPart1(inputLines []string) {
 }
 
 func RunPart2(inputLines []string) {
+	fmt.Println("O2")
 	oxygen := findOxygen(inputLines)
+	fmt.Println("CO2")
 	co2 := findC02(inputLines)
 	fmt.Println(oxygen)
 	fmt.Println(co2)
@@ -64,20 +66,38 @@ func findC02(inputLines []string) int {
 	for i := 0; i <= len(inputLines[0])-1; i++ {
 		zeros := make([]string, 0)
 		ones := make([]string, 0)
+		//fmt.Println("current")
+		//fmt.Println(current)
 		for j := 0; j <= len(current)-1; j++ {
 			if current[j][i] == '0' {
-				ones = append(ones, current[j])
-			} else {
 				zeros = append(zeros, current[j])
+			} else {
+				ones = append(ones, current[j])
 			}
+		}
+		if len(ones) == len(zeros) {
+			fmt.Println(i)
+			fmt.Println(current)
 		}
 		if len(ones) <= len(zeros) {
 			current = make([]string, len(ones))
 			copy(current, ones)
+		} else if len(ones) == len(zeros) && len(ones) == 1 && len(zeros) == 1 {
+			for _, val := range current {
+				//fmt.Println(current)
+				if val[i] == '0' {
+					//fmt.Println(val)
+					co2 := decimalFromBinary(val)
+					return int(co2)
+				}
+			}
 		} else {
 			current = make([]string, len(zeros))
 			copy(current, zeros)
 		}
+		//if len(current) < 5 {
+		//	fmt.Println(current)
+		//}
 		if len(current) == 1 {
 			co2 := decimalFromBinary(current[0])
 			return int(co2)
@@ -89,24 +109,37 @@ func findC02(inputLines []string) int {
 func findOxygen(inputLines []string) int {
 	current := make([]string, len(inputLines))
 	copy(current, inputLines)
+
 	for i := 0; i <= len(inputLines[0])-1; i++ {
 		zeros := make([]string, 0)
 		ones := make([]string, 0)
+		fmt.Println("current")
+		fmt.Println(current)
 		for j := 0; j <= len(current)-1; j++ {
 			if current[j][i] == '0' {
-				ones = append(ones, current[j])
-			} else {
 				zeros = append(zeros, current[j])
+			} else {
+				ones = append(ones, current[j])
 			}
 		}
 		if len(ones) >= len(zeros) {
 			current = make([]string, len(ones))
 			copy(current, ones)
+		} else if len(ones) == len(zeros) && len(ones) == 1 && len(zeros) == 1 {
+			for _, val := range current {
+				if val[i] == '1' {
+					oxygen := decimalFromBinary(val)
+					return int(oxygen)
+				}
+			}
 		} else {
 			current = make([]string, len(zeros))
 			copy(current, zeros)
 		}
-		fmt.Println(current[0])
+
+		//if len(current) < 5 {
+		//	fmt.Println(current)
+		//}
 		if len(current) == 1 {
 			oxygen := decimalFromBinary(current[0])
 			return int(oxygen)
