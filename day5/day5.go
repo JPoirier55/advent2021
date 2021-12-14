75f9632a35e7bb6bc219a70b043dcaf102dfeb12
Jake Poirier <jake.poirier@stash.com>
2021-12-13 21:24:55 -0500

    Dont care, close enough

--- day5/day5.go
+++ day5/day5.go
@@ -45,50 +45,117 @@ func Vectorize(line string) Vector {
 	return vector
 }
 
-func markPoints(vector Vector, boardMap map[string]int) {
-
+func markPoints(vector Vector, boardMap map[string]int, part2 bool) {
+	// ex 9,7 -> 7,9
 	startX := vector.start[0]
 	startY := vector.start[1]
 	endX := vector.end[0]
 	endY := vector.end[1]
+	x1 := vector.start[0]
+	y1 := vector.start[1]
+	x2 := vector.end[0]
+	y2 := vector.end[1]
+
 	if !(startX == endX || startY == endY) {
-		return
-	}
-	if vector.start[0] > vector.end[0] {
-		 startX = vector.end[0]
-		 endX = vector.start[0]
-	}
-	if vector.start[1] > vector.end[1] {
-		startY = vector.end[1]
-		endY = vector.start[1]
-	}
-	x := startX
-	for x <= endX {
-		y := startY
-		for y <= endY {
-			boardMap[strconv.Itoa(x)+","+strconv.Itoa(y)] +=1
-			y++
+		if !part2 {
+			return
 		}
-		x++
+		if x1 < x2 {
+			if y1 < y2 {
+				// x,y increase
+				//fmt.Println("x,y increase")
+				x := x1
+				y := y1
+				for x < x2 {
+					addToBoard(x,y,boardMap)
+					y++
+					x++
+				}
+			} else {
+				// x increases, y decreases
+				//fmt.Println("x increases y decrease")
+				x := x1
+				y := y1
+				for x < x2 {
+					addToBoard(x,y,boardMap)
+					y--
+					x++
+				}
+			}
+		} else {
+			if y1 < y2 {
+				// y increases, x decreases
+				//fmt.Println("y increase, x decrease")
+				x := x1
+				y := y1
+				for x > x2 {
+					addToBoard(x,y,boardMap)
+					y++
+					x--
+				}
+			} else {
+				//fmt.Println("both decrease")
+				x := x1
+				y := y1
+				for x > x2 {
+					addToBoard(x,y,boardMap)
+					y--
+					x--
+				}
+			}
+		}
+	} else {
+		if vector.start[0] > vector.end[0] {
+			startX = vector.end[0]
+			endX = vector.start[0]
+		}
+		if vector.start[1] > vector.end[1] {
+			startY = vector.end[1]
+			endY = vector.start[1]
+		}
+		x := startX
+		for x <= endX {
+			y := startY
+			for y <= endY {
+				addToBoard(x,y,boardMap)
+				y++
+			}
+			x++
+		}
 	}
 }
 
-func RunPart1(inputLines []string) {
-	boardMap := make(map[string]int)
-	for _, line := range inputLines {
-		vector := Vectorize(line)
-		markPoints(vector, boardMap)
-	}
+func addToBoard(x int, y int, boardMap map[string]int) {
+	//fmt.Print("x: ", x)
+	//fmt.Print(" - y: ",y)
+	//fmt.Println()
+	boardMap[strconv.Itoa(x)+","+strconv.Itoa(y)] +=1
+}
+
+func sumBoard(boardMap map[string]int) int {
 	sum := 0
 	for _, point := range boardMap {
 		if point > 1  {
 			sum++
 		}
 	}
-	fmt.Println(sum)
+	return sum
+}
 
+func RunPart1(inputLines []string) {
+	boardMap := make(map[string]int)
+	for _, line := range inputLines {
+		vector := Vectorize(line)
+		markPoints(vector, boardMap, false)
+	}
+	fmt.Println(sumBoard(boardMap))
 }
 
 func RunPart2(inputLines []string) {
-
+	boardMap := make(map[string]int)
+	for _, line := range inputLines {
+		vector := Vectorize(line)
+		markPoints(vector, boardMap, true)
+	}
+	fmt.Println(sumBoard(boardMap))
 }
