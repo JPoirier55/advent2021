fb02e8fcd21c4eecadbdad7fc8f15e6923a4f22f
Jake Poirier <jake.poirier@stash.com>
2021-12-17 21:01:15 -0500

    Day 9 pt 2

--- day9/day9.go
+++ day9/day9.go
@@ -2,7 +2,9 @@ package day9
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
+	"time"
 )
 
 func check(e error) {
@@ -35,7 +37,7 @@ func RunPart1(inputLines []string) {
 	fmt.Println(sum)
 }
 
-func getPointMap(inputLines []string) []Point{
+func getPointMap(inputLines []string) []*Point{
 	lvlMap := make([][]*Point, len(inputLines))
 	for i := range lvlMap {
 		lvlMap[i] = make([]*Point, len(inputLines[0]))
@@ -47,7 +49,7 @@ func getPointMap(inputLines []string) []Point{
 			lvlMap[i][j] = &Point{value: temp, basin: -1, visited: false}
 		}
 	}
-	points := make([]Point, 0)
+	points := make([]*Point, 0)
 	for y, line := range lvlMap {
 		for x, point := range line {
 			if y == 0 {
@@ -74,7 +76,7 @@ func getPointMap(inputLines []string) []Point{
 			} else {
 				point.right = lvlMap[y][x+1]
 			}
-			points = append(points, *point)
+			points = append(points, point)
 		}
 	}
 	return points
@@ -89,61 +91,7 @@ func printPoint(point Point) {
 	fmt.Println()
 }
 
-func markBasin(point *Point, basin int) bool {
-	debug := false
-	point.visited = true
-	if point.up != nil && point.up.value != 9 && point.up.basin == -1 && !point.up.visited{
-		if debug { fmt.Println("going up") }
-		markBasin(point.up, basin)
-	} else {
-		if debug { fmt.Println("no more up") }
-		point.basin = basin
-	}
-	if point.down != nil && point.down.value != 9 && point.down.basin == -1 && !point.down.visited{
-		if debug {fmt.Println("going down")}
-		markBasin(point.down, basin)
-	} else {
-		if debug {fmt.Println("no more down")}
-		point.basin = basin
-	}
-	if point.left != nil && point.left.value != 9 && point.left.basin == -1 && !point.left.visited{
-		if debug {fmt.Println("going left")}
-		markBasin(point.left, basin)
-	} else {
-		if debug {fmt.Println("no more left")}
-		point.basin = basin
-	}
-	if point.right != nil && point.right.value != 9 && point.right.basin == -1 && !point.right.visited{
-		if debug {fmt.Println("going right")}
-		markBasin(point.right, basin)
-	} else {
-		if debug {fmt.Println("no more right")}
-		point.basin = basin
-	}
-	if debug {
-		fmt.Println(point)
-		fmt.Println(point.basin)
-	}
-	return true
-}
-
-func RunPart2(inputLines []string) {
-	pointMap := getPointMap(inputLines)
-	currentBasin := 0
-	basinMap := make(map[int]int)
-	basins := make([]int, 0)
-	for _, pt := range pointMap {
-		pt2 := pt
-		if pt2.value != 9 && pt2.basin == -1 && !pt2.visited{
-			markBasin(&pt2, currentBasin)
-		}
-		if pt2.right != nil && pt2.right.basin == -1 && pt2.value == 9 && pt2.right.value != 9{
-			currentBasin++
-		}
-		fmt.Println(pt2)
-		basinMap[pt2.basin]++
-		basins = append(basins, pt2.basin)
-	}
+func printBasins(basins []int) {
 	count := 0
 	for _, y := range basins {
 		if y == -1 {
@@ -158,15 +106,85 @@ func RunPart2(inputLines []string) {
 			count = 0
 		}
 	}
+}
 
-	fmt.Println(basinMap)
-	result := 1
-	for i, t := range basinMap {
-		if i != -1 {
-			result *= t
+func markBasin(point *Point, basin int) {
+	debug := false
+	p2 := point
+	p2.visited = true
+	if debug { time.Sleep(1 * time.Second) }
+
+	if p2.up != nil && p2.up.value != 9 && p2.up.basin == -1 && !p2.up.visited{
+		if debug { fmt.Println("going up") }
+		markBasin(p2.up, basin)
+	} else {
+		if p2.basin == -1 {
+			p2.basin = basin
+		}
+		if debug { fmt.Println("no more up") }
+	}
+	if p2.down != nil && p2.down.value != 9 && p2.down.basin == -1 && !p2.down.visited{
+		if debug {fmt.Println("going down")}
+		markBasin(p2.down, basin)
+	} else {
+		if debug {fmt.Println("no more down")}
+		if p2.basin == -1 {
+			p2.basin = basin
 		}
 	}
+	if p2.left != nil && p2.left.value != 9 && p2.left.basin == -1 && !p2.left.visited{
+		if debug {fmt.Println("going left")}
+		markBasin(p2.left, basin)
+	} else {
+		if debug {fmt.Println("no more left")}
+		if p2.basin == -1 {
+			p2.basin = basin
+		}
+	}
+	if p2.right != nil && p2.right.value != 9 && p2.right.basin == -1 && !p2.right.visited{
+		if debug {fmt.Println("going right")}
+		markBasin(p2.right, basin)
+	} else {
+		if debug {fmt.Println("no more right")}
+		if p2.basin == -1 {
+			p2.basin = basin
+		}
+	}
+	if debug {
+		fmt.Println(p2)
+		fmt.Println(p2.basin)
+	}
+}
+
+func RunPart2(inputLines []string) {
+	pointMap := getPointMap(inputLines)
+	currentBasin := 0
+	basinMap := make(map[int]int)
+	basins := make([]int, 0)
+	for _, pt := range pointMap {
+		pt2 := pt
+		if pt2.value != 9 && pt2.basin == -1 && !pt2.visited{
+			markBasin(pt2, currentBasin)
+		}
+		if pt2.right != nil && pt2.right.basin == -1 && pt2.value == 9 && pt2.right.value != 9{
+			currentBasin++
+		}
+		basinMap[pt2.basin]++
+		basins = append(basins, pt2.basin)
+	}
+	//printBasins (basins)
+
+	arr := make([]int, 0)
+	for i, t := range basinMap {
+		if i != -1 {
+			arr = append(arr, t)
+		}
+	}
+	sort.Ints(arr)
+	finals := arr[len(arr)-3:]
+	result := 1
+	for _, val := range finals {
+		result *= val
+	}
 	fmt.Println(result)
-
-
 }
